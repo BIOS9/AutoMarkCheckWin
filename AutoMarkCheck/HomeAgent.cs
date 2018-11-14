5fd47ef6ac9d648cd0afe675133ffd92d4e4582d
NightFish <NightFish.net@gmail.com>
2018-11-15 03:36:51 +1300

    Started adding the home agent

---
+++ AutoMarkCheck/HomeAgent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoMarkCheck
+{
+    /**
+     * <summary>Agent to interface with the home bot server.</summary>
+     */
+    public class HomeAgent
+
+    {
+    }
+}
