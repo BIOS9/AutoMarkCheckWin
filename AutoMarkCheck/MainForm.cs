26015e6b27ac7354c867f8b98ecf34cc3967d2f1
NightFish <NightFish.net@gmail.com>
2018-11-14 17:44:20 +1300

    Added method to get login UUID and cookies

--- AutoMarkCheck/MainForm.cs
+++ AutoMarkCheck/MainForm.cs
@@ -4,7 +4,9 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +14,19 @@ namespace AutoMarkCheck
 {
     public partial class MainForm : Form
     {
+        private const string BASE_URL = "https://my.vuw.ac.nz";
+        private const string LOGIN_PAGE_PATH = "/cp/home/displaylogin";
+        private const string LOGIN_POST_PATH = "/cp/home/login";
+        private const string LOGIN_UUID_PATTERN = "(?:document.cplogin.uuid.value=\")([\\da-zA-Z-]+)(?:\";)";
+
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            await MyVUWAgent.GetGrades(null);
         }
     }
 }
--- AutoMarkCheck/MyVUWAgent.cs
+++ AutoMarkCheck/MyVUWAgent.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -7,6 +8,7 @@ using System.Security.Authentication;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using static AutoMarkCheck.CredentialManager;
 
 namespace AutoMarkCheck
@@ -27,34 +29,62 @@ namespace AutoMarkCheck
         private const string LOGIN_POST_PATH = "/cp/home/login";
         private const string LOGIN_UUID_PATTERN = "(?:document.cplogin.uuid.value=\")([\\da-zA-Z-]+)(?:\";)";
 
-        public async Task<IList<CourseGrade>> GetGrades(MarkCredentials credentials)
+        public static async Task<IList<CourseGrade>> GetGrades(MarkCredentials credentials)
         {
-
+            CookieCollection cookies = await Login(credentials);
             return new List<CourseGrade>();
         }
 
-        private async Task<CookieCollection> Login(MarkCredentials credentials)
+        private static async Task<CookieCollection> Login(MarkCredentials credentials)
         {
             try
             {
-                WebClient wc = new WebClient();
-                string pageText = await wc.DownloadStringTaskAsync(BASE_URL + LOGIN_PAGE_PATH);
-                string uuid = Regex.Match(pageText, LOGIN_UUID_PATTERN).Groups[1].Value;
+                Tuple<string, CookieCollection> loginParams = await GetLoginParams();
+                MessageBox.Show(loginParams.Item1);
+                MessageBox.Show(loginParams.Item2.Count.ToString());
+
+                string postdata = "pass=aaa&user=asdasdas&uuid=" + loginParams.Item1;
+                var data = Encoding.ASCII.GetBytes(postdata);
 
                 HttpWebRequest request = WebRequest.CreateHttp(BASE_URL + LOGIN_POST_PATH);
                 request.Method = "POST";
                 request.ContentType = "application/x-www-form-urlencoded";
+                request.ContentLength = data.Length;
+                request.CookieContainer = new CookieContainer();
+                request.CookieContainer.Add(loginParams.Item2); //Add the cookies from the login parameters to the request
 
-                using (var stream = await request.GetRequestStreamAsync())
+                using (Stream stream = await request.GetRequestStreamAsync())
                 {
-
+                    await stream.WriteAsync(data, 0, data.Length);
                 }
 
-                HttpWebResponse response = await request.GetResponseAsync() as HttpWebResponse;
+                using (HttpWebResponse response = await request.GetResponseAsync() as HttpWebResponse)
+                {
+                    string respStr = await new StreamReader(response.GetResponseStream()).ReadToEndAsync();
+                    return null;
+                }
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 throw new AuthenticationException("Failed to login to MyVictoria: " + ex.Message, ex); //Throw login failure exception with the inner exception
+            }
+        }
+
+        private static async Task<Tuple<string, CookieCollection>> GetLoginParams()
+        {
+            HttpClient hc = new HttpClient();
+            HttpWebRequest request = WebRequest.CreateHttp(BASE_URL + LOGIN_PAGE_PATH);
+            request.Method = "GET";
+            request.CookieContainer = new CookieContainer(); //Create somewhere for the cookies to go
+
+            using (HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync()) //Get page data
+            using (Stream stream = response.GetResponseStream()) //Get the data stream from the response
+            using (StreamReader reader = new StreamReader(stream)) //Create reader to read HTML from the data stream
+            {
+                string pageText = await reader.ReadToEndAsync();
+                string uuid = Regex.Match(pageText, LOGIN_UUID_PATTERN).Groups[1].Value; //Find the UUID inside the HTML/JS
+
+                return new Tuple<string, CookieCollection>(uuid, response.Cookies); //Return UUID and cookies
             }
         }
     }
