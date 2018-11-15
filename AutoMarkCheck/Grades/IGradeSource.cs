726f011f7b6ddc980ccf0ec94d03183cbb757821
NightFish <NightFish.net@gmail.com>
2018-11-15 23:33:27 +1300

    Added some documentation to IGradeSource

--- AutoMarkCheck/Grades/IGradeSource.cs
+++ AutoMarkCheck/Grades/IGradeSource.cs
@@ -7,6 +7,9 @@ using static AutoMarkCheck.Helpers.CredentialManager;
 
 namespace AutoMarkCheck.Grades
 {
+    /**
+     * <summary>Interface for grade sources. Adds standardization to grade sources through enforced methods.</summary>
+     */
     interface IGradeSource
     {
         Task<List<CourseInfo>> GetGrades();
