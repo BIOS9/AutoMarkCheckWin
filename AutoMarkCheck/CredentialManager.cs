2e12349c7fd63e9eaeda113dda3da37d5f9c78fe
NightFish <NightFish.net@gmail.com>
2018-11-15 15:56:13 +1300

    Cleaned up usings

--- AutoMarkCheck/CredentialManager.cs
+++ AutoMarkCheck/CredentialManager.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.Security;
 using System.Text;
-using System.Windows.Forms;
 
 namespace AutoMarkCheck
 {
--- AutoMarkCheck/MainForm.cs
+++ AutoMarkCheck/MainForm.cs
@@ -1,15 +1,5 @@
-﻿using Newtonsoft.Json.Linq;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Net.Http;
-using System.Security;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using static AutoMarkCheck.MyVUWAgent;
 
