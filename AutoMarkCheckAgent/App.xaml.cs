fb7270d24cb3a2b91bb0daffc5dc09cdfd63e64b
NightFish <NightFish.net@gmail.com>
2018-11-17 19:02:13 +1300

    Removed background process code from C# app, to be moved to C++ daemon

--- AutoMarkCheckAgent/App.xaml.cs
+++ AutoMarkCheckAgent/App.xaml.cs
@@ -5,21 +5,44 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Forms;
+using static AutoMarkCheck.Helpers.Logging;
 
 namespace AutoMarkCheckAgent
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : System.Windows.Application
     {
-        public static MarkCheckDaemon Daemon;
-
         [STAThread]
-        public static void Main()
+        public static void Main(string[] args)
         {
             Logging.Initialize();
-            Daemon = new MarkCheckDaemon();
+            if(args.Contains("-gui"))
+            {
+                Show();
+            }
+            else
+            {
+
+            }
+        }
+
+        private static void Show()
+        {
+            try
+            {
+                Logging.Log(LogLevel.DEBUG, $"{nameof(AutoMarkCheckAgent)}.{nameof(App)}.{nameof(Show)}", "Opening GUI.");
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.ShowDialog();
+                Logging.Log(LogLevel.DEBUG, $"{nameof(AutoMarkCheckAgent)}.{nameof(App)}.{nameof(Show)}", "Closed GUI.");
+            }
+            catch (Exception ex)
+            {
+                Logging.Log(LogLevel.ERROR, $"{nameof(AutoMarkCheckAgent)}.{nameof(App)}.{nameof(Show)}", "Error loading GUI.", ex);
+                System.Windows.Forms.MessageBox.Show("There was an error loading the GUI: \n" + ex.Message + "\n\nCheck the log file for a more detailed error.", "Auto Mark Check Agent", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
